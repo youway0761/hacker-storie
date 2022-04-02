cd2ddce075390b0c439c5c694ceef9a3a820ab34
youway0761 <youway@hotmail.com>
2022-04-02 16:41:23 +0800

    for check syntx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { getByTitle } from "@testing-library/react";
 import React from "react";
 
 const welcome = {
@@ -10,12 +9,20 @@ function getTitle(title) {
   return title;
 }
 
+const wels = ['welcome', 'to', 'React', 'world', '!']
+
 function App() {
+  let strs = '';
+  for (let str of wels) {
+    strs += str + ' ';
+  }
 
   return (
     <div>
       <h1>{welcome.greeting} {welcome.title}</h1>
       <h1>Hello {getTitle('React')}</h1>
+      <h1>{wels[0]} {wels[1]} {wels[2]} {wels[3]}{wels[4]}</h1>
+      <h1>{strs}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
