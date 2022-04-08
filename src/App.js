3f24f3f0b3d9a507c84e7057d929131642880d18
youway0761 <youway@hotmail.com>
2022-04-08 11:48:06 +0800

    add onChange to input

--- src/App.js
+++ src/App.js
@@ -20,12 +20,15 @@ const list = [
   ];
   
 const App = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
    return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
 
       <hr/>
       <List/>
