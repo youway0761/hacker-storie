c8186d489dddd3fd1d3e6b0858cd8fadefc6c08f
youway0761 <youway@hotmail.com>
2022-04-08 12:03:55 +0800

    change list from global to local

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-  ];
+
   
 const App = () => {
-  const handleChange = (event) => {
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
+    
+    const handleChange = (event) => {
     console.log(event.target.value);
   }
-   return (
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -31,13 +33,13 @@ const App = () => {
       <input id="search" type="text" onChange={handleChange}/>
 
       <hr/>
-      <List/>
+      <List list={stories}/>
     </div>
   );
 }
 
-const List = () => 
-  list.map(item => 
+const List = (props) => 
+  props.list.map(item => 
     (
       <div key={item.objectID}>
         <span>
