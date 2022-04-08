2e4e88038e102904b595b6a52e82568a4011ec0e
youway0761 <youway@hotmail.com>
2022-04-08 21:46:09 +0800

    add callback between component

--- src/App.js
+++ src/App.js
@@ -5,45 +5,57 @@ import React from "react";
 const App = () => {
   const stories = [
     {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
     },
     {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
     },
-    ];
-    
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  }
-
-  const [searchTerm, setSearchTerm] = React.useState('');
-
+  ];
+  
+      // A
+  const handleSearch = event => {
+    // C
+    console.log(event.target.value);
+  };
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-      
+      <Search onSearch={handleSearch}/>
       <hr/>
       <List list={stories}/>
     </div>
   );
 }
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    //B
+    // eslint-disable-next-line react/prop-types
+    props.onSearch(event);
+  };
+ 
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
 
 const List = (props) => 
   props.list.map(item => 
