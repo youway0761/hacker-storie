ffc8c924c473b01038925f37deb32b645cf02ce5
youway0761 <youway@hotmail.com>
2022-04-09 09:18:02 +0800

    state be upgrade from Search to App

--- src/App.js
+++ src/App.js
@@ -22,36 +22,29 @@ const App = () => {
     },
   ];
 
-  // A
+  const [searchTerm, setSearchTerm] = React.useState('');
   const handleSearch = event => {
-    // C
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-    //B
-    props.onSearch(event);
-  };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   );
 };
