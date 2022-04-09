9c8a6560fe3d852a108468aaca0044261d0e63a9
youway0761 <youway@hotmail.com>
2022-04-09 15:32:14 +0800

    add Fragment <>

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const App = () => {
   );
 }
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input
       id="search"
@@ -60,7 +60,7 @@ const Search = ({ search, onSearch }) => (
       value={search}
       onChange={onSearch}
     />
-  </div>
+  </>
 );
 
 
