10800b2156194b275bdc52067c2192d31b954e80
youway0761 <youway@hotmail.com>
2022-04-10 12:51:31 +0800

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -44,11 +44,17 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
+
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
@@ -80,7 +86,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 }
