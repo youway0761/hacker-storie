1405c8c20752a51c793c867430c75df71d8876c6
youway0761 <youway@hotmail.com>
2022-04-11 11:38:47 +0800

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import axios from "axios";
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+height: 100vw;
+padding: 20px;
+background: #83a4d4;
+background: linear-gradient(to left, #b6fbff, #83a4d4);
+color: #171212;
+`;
+const StyledHeadlinePrimary = styled.h1`
+font-size: 48px;
+font-weight: 300;
+letter-spacing: 2px;
+`;
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -115,8 +129,8 @@ const App = () => {
   //story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -131,7 +145,7 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </div>
+    </StyledContainer>
   );
 }
 
