4eb44952add75d1528e65ce3998d9531d6d88250
youway0761 <youway@hotmail.com>
2022-04-14 11:38:46 +0800

    add sort for list

--- src/List.js
+++ src/List.js
@@ -2,9 +2,55 @@ import React from 'react';
 import { StyledColumn, StyledButtonSmall } from './style';
 import styles from "./App.module.css";
 import { ReactComponent as Check } from './check.svg';
+import { sortBy } from 'lodash';
 
-const List = ({ list, onRemoveItem }) =>
-    list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
+const SORTS = {
+    NONE: list => list,
+    TITLE: list => sortBy(list, 'title'),
+    AUTHOR: list => sortBy(list, 'author'),
+    COMMENT: list => sortBy(list, 'num_comments').reverse(),
+    POINT: list => sortBy(list, 'points').reverse(),
+};
+
+const List = ({ list, onRemoveItem }) => {
+    const [sort, setSort] = React.useState('NONE');
+    const handleSort = sortKey => {
+        setSort(sortKey);
+    };
+    const sortFunction = SORTS[sort];
+    console.log(sortFunction);
+    const sortedList = sortFunction(list);
+    return (
+        <div>
+            <div style={{ display: 'flex' }}>
+                <span style={{ width: '40%' }}>
+                    <button type="button" onClick={() => handleSort('TITLE')}>
+                        Title
+                    </button>
+                </span>
+                <span style={{ width: '30%' }}>
+                    <button type="button" onClick={() => handleSort('AUTHOR')}>
+                        Author
+                    </button>
+                </span>
+                <span style={{ width: '10%' }}>
+                    <button type="button" onClick={() => handleSort('COMMENT')}>
+                        Comments
+                    </button>
+                </span>
+                <span style={{ width: '10%' }}>
+                    <button type="button" onClick={() => handleSort('POINT')}>
+                        Points
+                    </button>
+                </span>
+                <span style={{ width: '10%' }}>Actions</span>
+            </div>
+            {sortedList.map(item => (
+                <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+            ))};
+        </div>
+    );
+};
 
 const Item = ({ item, onRemoveItem }) => {
     return (
